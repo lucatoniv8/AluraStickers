ef751b2c7e886c5656afe8dbb6b4de74ea179657
lucatoniv8 <lucatoniv8@gmail.com>
2023-03-31 15:14:41 -0300

    aula 3 finalizada

--- src/App.java
+++ src/App.java
@@ -1,86 +1,35 @@
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
 
-        // menu aplicação
-        System.out.println("Escolha qual Rank você deseja:");
-        System.out.println("|1 - Filmes|-|2 - Series|-|0 - Sair|");
-        Scanner ler = new Scanner(System.in);
-        int opcao = ler.nextInt();
-        String url = "";
-        switch (opcao) {
-            case 1:
-            url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-                break;
-            case 2:
-            url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";
-                break;
-            case 0:
-                System.out.println("Saindo ..........");
-                break;
-            default:
-            System.out.println("Não entendi fechando aplicação");
-                break;
-        }
-        // fazer um conexão HTTP e buscar os top 250 filmes
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-        
-        // extrair os dados que interessam (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        
-        var diretorio = new File("figurinhas/");
-        diretorio.mkdir();
+        API api = API.IMDB_TOP_MOVIES;
+
+        String url = api.getUrl();
+        ExtratorDeConteudo extrator = api.getExtrator();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
+
+        // exibir e manipular os dados 
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
 
         var geradora = new GeradoraDeFigurinhas();
-        System.out.println("\n====================================================");
-        
-        // exibir e manipular os dados
-        for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println("\u001b[1mTITULO:\u001b[m" + filme.get("title"));
-            double estrelas = Double.parseDouble(filme.get("imDbRating"));
-            for (int i = 1; i <= estrelas; i++) {
-                System.out.print(" ⭐");
-            }
-            System.out.println(" "+estrelas);
-            String rotulo = "";
-            if (estrelas > 9){
-                rotulo = "OBRA PRIMA";
-            }else if(estrelas > 7){
-                rotulo = "MUITO BOM ";
-            }else if(estrelas > 5){
-                rotulo = "TÁ FRACO";
-            }else{
-                rotulo = "RUIM DEMAIS";
-            }
-            System.out.println(rotulo);
-            System.out.println("\n====================================================");
-            
-            String urlImagem = filme.get("image");
-            String titulo = filme.get("title");
 
-            InputStream inputStream = new URL(urlImagem).openStream();
-            InputStream joinha = new FileInputStream(new File("figurinhas/joinha.png"));
-            String nomeArquivo = "figurinhas/" + titulo + ".png";
-            
-            geradora.cria(inputStream, joinha, nomeArquivo, rotulo);
+        for (int i = 0; i < 3; i++) {
 
+            Conteudo conteudo = conteudos.get(i);
+
+            InputStream inputStream = new URL(conteudo.urlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.titulo() + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(conteudo.titulo());
+            System.out.println();
         }
     }
 }
--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,13 +1,6 @@
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics2D;
-import java.awt.Shape;
-import java.awt.font.FontRenderContext;
-import java.awt.font.TextLayout;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
@@ -16,57 +9,38 @@ import javax.imageio.ImageIO;
 
 public class GeradoraDeFigurinhas {
     
-    public void cria( InputStream inputStream, InputStream joinha, String nomeArquivo, String rotulo ) throws Exception{
+
+    public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
 
         // leitura da imagem
+        // InputStream inputStream = 
+        //             new FileInputStream(new File("entrada/filme-maior.jpg"));
+        // InputStream inputStream = 
+        //                 new URL("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@.jpg")
+        //                 .openStream();
         BufferedImage imagemOriginal = ImageIO.read(inputStream);
-        BufferedImage joinhaSorrindo = ImageIO.read(joinha);
 
-        // criar nova imagem em memoria com transparencia com tamanho novo
+        // cria nova imagem em memória com transparência e com tamanho novo
         int largura = imagemOriginal.getWidth();
         int altura = imagemOriginal.getHeight();
         int novaAltura = altura + 200;
         BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
 
-        //copiar a imagem original para nova imagem em memoria
-        Graphics2D graphics = (Graphics2D) novaImagem.createGraphics();
+        // copiar a imagem original pra novo imagem (em memória)
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOriginal, 0, 0, null);
 
-        int alturaJoinha = altura - joinhaSorrindo.getHeight();
-        graphics.drawImage(joinhaSorrindo, 0, alturaJoinha, null);
-        
         // configurar a fonte
-        var fonte = new Font("Impact", Font.BOLD, 90);
-        graphics.setColor(Color.ORANGE);
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.YELLOW);
         graphics.setFont(fonte);
-        
-        //escrever um texto na imagem
-        FontMetrics fontMetrics = graphics.getFontMetrics();
-        Rectangle2D retangulo = fontMetrics.getStringBounds(rotulo, graphics);
-        int larguraTexto = (int)retangulo.getWidth();       
-        int posicaoTextoX = (largura - larguraTexto)/2;
-        int posicaoTextoY = novaAltura - 50;
-        graphics.drawString(rotulo, posicaoTextoX, posicaoTextoY);
 
-        // fazer contorno na fonte
-        FontRenderContext fontRenderContext = graphics.getFontRenderContext();
-        var textLayout = new TextLayout(rotulo, fonte, fontRenderContext);
+        // escrever uma frase na nova imagem
+        graphics.drawString("TOPZERA", 100, novaAltura - 100);
 
-        Shape outline = textLayout.getOutline(null);
-        AffineTransform transform = graphics.getTransform();
-        transform.translate(posicaoTextoX, posicaoTextoY);
-        graphics.setTransform(transform);
-
-        var outlineStroke = new BasicStroke(largura * 0.004f);
-        graphics.setStroke(outlineStroke);
-
-        graphics.setColor(Color.black);
-        graphics.draw(outline);
-        graphics.setClip(outline);
-
-
-        //escrever a nova imagem em um arquivo
+        // escrever a nova imagem em um arquivo
         ImageIO.write(novaImagem, "png", new File(nomeArquivo));
 
     }
+
 }
